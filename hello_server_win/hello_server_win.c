068fc2bd3d4aefa13bface5365944dc783a5b509
informatica0422 <informatica0422@gmail.com>
2023-04-18 23:31:06 +0900

    [2023-04-18] win용 socket학습

--- hello_server_win/hello_server_win.c
+++ hello_server_win/hello_server_win.c
@@ -19,4 +19,36 @@ int main(int argc, char* argv[])
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
 		ErrorHandling("WSAStartup() error!");
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServSock == INVALID_SOCKET)
+		ErrorHandling("socket() error!");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error!");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen error!");
+
+	szClnAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClnAddr);
+	if (hClntSock == INVALID_SOCKET)
+		ErrorHandling("accept() error!");
+
+	send(hClntSock, message, sizeof(message), 0);
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
